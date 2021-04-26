833ee923adb6c0d94385a1a2c9ed23e1e839cd8b
Kat <66186293+kattakatkatherine@users.noreply.github.com>
2021-04-26 10:29:49 -0500

    Update caesar.hs

--- caesar.hs
+++ caesar.hs
@@ -13,7 +13,6 @@ rotChar c n
  where a = c + n
 
 
-decodeGrid = [0,3,-1,1,1,4,0,0,2,2,-5,-2,1,0,2,3,0,-6,2,2,3,1,-1,0,-5,0,-7]
 decode :: [Char] -> [Char]
 decode s = decode' s 1 0
 decode' s n h
@@ -21,7 +20,8 @@ decode' s n h
  | score s n > score s h = decode' s (n+1) n 
  | otherwise = decode' s (n+1) h
 
-score s n = sum (map (\x -> decodeGrid!!(toOrd x)) $ caesar s n)
+score s n = sum $ map (\x -> scoreGrid!!(toOrd x)) $ caesar s n
+scoreGrid = [0,3,-1,1,1,4,0,0,2,2,-5,-2,1,0,2,3,0,-6,2,2,3,1,-1,0,-5,0,-7]
 toOrd c
  | a > 96 && a <= 122 = a - 96
  | a > 64 && a <= 90  = a - 64
